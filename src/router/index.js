aabdef0eed52d6da3e84825ebf0bc1b1e58c45ac
wanger12 <137703755@qq.com>
2023-07-10 10:47:24 +0800

    Initial commit

---
+++ src/router/index.js
@@ -0,0 +1,53 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/index/index.vue'
+
+Vue.use(VueRouter)
+
+const routes = [{
+    path: '/',
+    name: 'home',
+    component: Home,
+    // redirect: '/print',
+    children: [
+        {
+            path: 'print',
+            name: 'print',
+            component: () =>
+                import('@/views/vue-print-nb/vue-print-nb.vue')
+        },
+        {
+            path: 'table-excel',
+            name: 'table-excel',
+            component: () =>
+                import('@/views/table-excel/table-excel.vue')
+        },
+        {
+            path: 'file-preview',
+            name: 'file-preview',
+            component: () =>
+                import('@/views/file-preview/file-preview.vue')
+        },
+    ]
+
+},
+
+    // {
+    //     path: '/table-excel',
+    //     name: 'table-excel',
+    //     component: () =>
+    //         import('@/views/vue-plugin-hiprint/index.vue')
+    // },
+    // {
+    //     path: '/table-excel',
+    //     name: 'table-excel',
+    //     component: () =>
+    //         import('@/views/table-excel/table-excel.vue')
+    // },
+]
+
+const router = new VueRouter({
+    routes
+})
+
+export default router
---
+++ src/views/index/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <a-button>
+            <router-link class="navbar-brand" to="print">print打印</router-link>
+        </a-button>
+        <a-button>
+            <router-link class="navbar-brand" to="table-excel">table-excel导出</router-link>
+        </a-button>   
+        <a-button>
+            <router-link class="navbar-brand" to="file-preview">文件预览</router-link>
+        </a-button>    
+        <a-button>
+            <router-link class="navbar-brand" to="file-preview">高德地图</router-link>
+        </a-button>    
+        <router-view></router-view>    
+    </div>  
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
